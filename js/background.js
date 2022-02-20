230a994bf12eb1d67c878964af973f194cfc853b
JSND-OJ <89259775+JSND-OJ@users.noreply.github.com>
2022-02-20 23:18:10 +0900

    too tired to fix. maybe later

--- js/background.js
+++ js/background.js
@@ -2,8 +2,6 @@ const images = ["1.gif", "2.gif", "3.gif", "4.gif", "5.gif"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
-
-bgImage.src = `img/${chosenImage}`;
-
-document.body.appendChild(bgImage);
+document.body.style.backgroundImage = `url('img/${chosenImage}')`
+document.body.style.backgroundRepeat = 'no-repeat'
+document.body.style.backgroundSize = 'cover'
